cfef2de581036842899320ec56a7b03332ecd2df
Genilson <genilson255@hotmail.com>
2022-02-05 17:37:56 -0300

    Fiz os primeiros cinco exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/arquivo.js
@@ -0,0 +1,58 @@
+
+
+
+/*
+const a = 10;
+const b = 20;  
+if (a != b) {
+    soma = a + b;
+    console.log("O numero encontrado foi", soma);    
+}else{
+    soma = a - b;
+    console.log("O numro encontrado foi", soma);
+} */
+
+/*     
+    
+    const a = 1;
+    const b = 2;
+    if (a === b) {
+        subtracao = a - b;
+        console.log("Este foi o resultado da subtracao", subtracao);            
+    } else {
+        subtracao = b - a; 
+        console.log("Este foi o resultado da subtracao", subtracao); 
+    }  */  
+   /*      let = numero = 7;
+        let multiplicacao = 1;
+        const a = 10;
+        const b = 5;
+        if (multiplicacao === numero) {
+            multiplicacao = a * b;   
+            console.log(multiplicacao);     
+        }else{
+            multiplicacao = a + multiplicacao;
+            console.log(multiplicacao);
+        } */
+           
+     /*    let divisao = 100;
+        const a = 50;
+        const b = 20;
+        let subtotal;
+
+       if (divisao / 10 != 1) {
+           subtotal = a / b;
+           console.log("Resultado obtido foi: ", subtotal);
+       } else if( divisao / 10 === 10) {
+        subtotal = b / a;
+        console.log("Resultado da divisao foi:", subtotal);
+       }else {
+
+           console.log("O resultado encontrado esta ");
+
+       }
+         */
+        
+        
+        /* let modulo = 2; */
+ 
