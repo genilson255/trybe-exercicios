34037161106d1de149698140f0c11556dc00a55d
Genilson <genilson255@hotmail.com>
2022-02-07 15:07:16 -0300

    Completei o exec 07

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/exec07.js
@@ -0,0 +1,40 @@
+
+
+/* 
+//soma
+const a = 10;
+const b = 20;  
+let soma;
+soma = a + b;
+console.log(soma);
+ */
+  /*   
+   //subtracao 
+const a = 1;
+const b = 2;
+let subtracao;
+subtracao = a - b;
+console.log(subtracao);    */
+
+/* 
+const a = 10;
+const b = 5;
+let multiplicacao;
+multiplicacao = a * b;
+console.log(multiplicacao); */
+/* 
+    //divisao
+const a = 10;
+const b = 5;
+let divisao;
+divisao = a / b;
+console.log(divisao); */
+
+/* 
+//Modulo
+const a = 11;
+const b = 5;
+let modulo;
+modulo = a % b;
+console.log(modulo); */
+ 
