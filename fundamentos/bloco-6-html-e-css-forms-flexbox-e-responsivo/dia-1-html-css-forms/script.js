b76f95b9ec5130d21218f8a4fb1bf3a359e19cb1
Genilson <genilson255@hotmail.com>
2022-02-19 11:59:13 -0300

    Atualizaçao

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,36 @@
+/* const button = document.querySelector('#buttonSubmit');
+let inputNameUser = document.querySelector('#inputNameUser');
+const nameUser = document.querySelector('#nameUser');
+
+function obterValorUuario(event){
+     event.preventDefault(); 
+    console.log(inputNameUser.value);
+    nameUser.innerHTML = inputNameUser.value;
+}
+button.addEventListener('click', obterValorUuario); */
+
+
+// SELETORES
+/* const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+
+
+    document.querySelector("#href").addEventListener("click", newClick(){
+    document.getElementById("input-checkbox");
+    event.preventDefault();
+}); */
+
+/* const texto = document.querySelector('#input-text');
+
+
+const caixa = document.querySelector('#input-checkbox');
+const botao = document.querySelector('#href');
+ acao.addEventListener('click', findClick());
+
+function findClick(){
+    console.log(click);
+      
+}
+ */
