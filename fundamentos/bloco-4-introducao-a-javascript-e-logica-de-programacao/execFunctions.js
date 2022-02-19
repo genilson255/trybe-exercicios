9f9dd6571b614387a7c5f471f3aed98cf2976169
Genilson <genilson255@hotmail.com>
2022-02-19 18:19:39 -0300

    update

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/execFunctions.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/execFunctions.js
@@ -5,7 +5,7 @@ function generateRandomNumer() {
     let numberRandom = Math.floor(Math.random() * 60) + 1;
     return numberRandom;
 }
-//console.log('O numero sorteado foi:', generateRandomNumer());
+console.log('O numero sorteado foi:', generateRandomNumer());
 
 // 2 Gerar um jogo ( Sortear os 6 numeros )
 function gerateGame() {
