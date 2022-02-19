54d3a6f2672506e860993cbc3055b19835fe972e
Genilson <genilson255@hotmail.com>
2022-02-19 18:12:48 -0300

    Concerto de erro do git

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/execFunctions.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/execFunctions.js
@@ -1,20 +1,20 @@
 //Refatorando o codigo
 // 1 Criar uma function para gerar um numero aleatório
 
-function gerarUmRandomNumber() {
-    let numeroAleatorio = Math.floor(Math.random() * 60) + 1;
-    return numeroAleatorio;
+function generateRandomNumer() {
+    let numberRandom = Math.floor(Math.random() * 60) + 1;
+    return numberRandom;
 }
-console.log('O numero sorteado foi:', gerarUmRandomNumber());
+//console.log('O numero sorteado foi:', generateRandomNumer());
 
 // 2 Gerar um jogo ( Sortear os 6 numeros )
 function gerateGame() {
-    let megaSenaGame = [];
+    let megaSenaJogo = [];
     for(let i = 0; 1 < 6; i += 1){
         /* megaSenaGame[i] = gerarUmRandomNumber(); */
-        megaSenaGame.push(gerarUmRandomNumber());
+        megaSenaJogo.push(generateRandomNumer());
     }
-    return megaSenaGame;
+    return megaSenaJogo;
 }
 console.log(gerateGame());
 
