8b0b4a940c59d55a01b259c4f9ec0125e087cc85
Genilson <genilson255@hotmail.com>
2022-02-21 12:20:35 -0300

    Reduçao de codigos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,4 +1,4 @@
-// 1 - Onde eu quero adicionar esse elemento?
+/* // 1 - Onde eu quero adicionar esse elemento?
 // Esse elemento existe no meu JS?
 let container = document.getElementById('first');
 // 2 Como criar um elemento HTML?
@@ -10,16 +10,35 @@ container.appendChild(newDiv);
 
 let newDiv2 = document.createElement('div');
 newDiv2.className = 'green circle small';
-container.appendChild(newDiv2);
+container.appendChild(newDiv2); */
 
 //Criação do elemento div
-let newDiv3 = document.createElement('div');
-newDiv3.className = 'yellow circle small';
+//let newDiv3 = document.createElement('div');
+//newDiv3.className = 'yellow circle small';
 //Adicinando o elemento
-container.appendChild(newDiv3);
+//container.appendChild(newDiv3);
 
 //Criação do elemento div
-let redondo = document.createElement('div');
-redondo.className = 'circle red small';
+//let redondo = document.createElement('div');
+//redondo.className = 'circle red small';
 //Adicinando o elemento
-container.appendChild(redondo);
+//container.appendChild(redondo);
+
+let container = document.getElementById('first');
+function createDivElement(classes){
+    let newDiv = document.createElement('div');
+    newDiv.className = classes;
+    console.log(newDiv);
+    return newDiv;
+}
+//Mandando os elementos para a o html
+/* container.appendChild(createDivElement('circle red small'));
+container.appendChild(createDivElement('circle yellow small'));
+container.appendChild(createDivElement('circle green small')); */
+
+function addElement(parent, child){
+    parent.appendChild(child);
+}
+addElement(container, createDivElement('circle red small'));
+/* addElement(container, createDivElement('circle green small'));
+addElement(container, createDivElement('circle yellow small')); */
