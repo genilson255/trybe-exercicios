92283f4b167053a60daff4d044228bf4bd38cb8e
Genilson <genilson255@hotmail.com>
2022-03-02 21:32:39 -0300

    Bootstrap excluidos

--- fundamentos/Bloco-07-Introdução-a-JavaScript-ES6-Testes-Unitarios/dia-1-JavaScript-ES6-let- const-arrow-functions-e-template-literals/script.js
+++ fundamentos/Bloco-07-Introdução-a-JavaScript-ES6-Testes-Unitarios/dia-1-JavaScript-ES6-let- const-arrow-functions-e-template-literals/script.js
@@ -28,7 +28,7 @@
 
 //FUNCAO para retornar um array em orde crescente ou decrescente
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-oddsAndEvens.sort((a, b) => b - a);
+oddsAndEvens.sort((a, b) => a - b);
 console.log(oddsAndEvens);
 
 /* function eventoSntigo(oddsAndEvens) { */
