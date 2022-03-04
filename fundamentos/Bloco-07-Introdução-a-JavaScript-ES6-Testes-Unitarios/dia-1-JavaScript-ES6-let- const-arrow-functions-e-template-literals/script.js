e83710cd1d46c819e7065ac75f30bdbcd0a59045
Genilson <genilson255@hotmail.com>
2022-03-04 18:51:40 -0300

    Adiciona a function fatorial

--- fundamentos/Bloco-07-Introdução-a-JavaScript-ES6-Testes-Unitarios/dia-1-JavaScript-ES6-let- const-arrow-functions-e-template-literals/script.js
+++ fundamentos/Bloco-07-Introdução-a-JavaScript-ES6-Testes-Unitarios/dia-1-JavaScript-ES6-let- const-arrow-functions-e-template-literals/script.js
@@ -1,1 +1,59 @@
 //Parte I
+
+/*
+Modifique a estrutura da função para que ela seja uma arrow function .
+Modifique as concatenações para template literals .
+Copie o código abaixo.
+*/
+/* function testingScope(escopo) { */
+/*   if (escopo === true) { */
+/*     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)'; */
+/*     ifScope = ifScope + ' ótimo, fui utilizada no escopo !'; */
+/*     console.log(ifScope); */
+/*   } else { */
+/*     var elseScope = 'Não devo ser utilizada fora meu escopo (else)'; */
+/*     console.log(elseScope); */
+/*   } */
+/*   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida. */
+/* } */
+/*  */
+/* testingScope(true); */
+/* const testingScope = escopo => {  */
+/*   if (escopo === true) {  */
+/*     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)'; */
+/*     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`; */
+/*     console.log(ifScope); */
+/*   } else { */
+/*     const elseScope = 'Não devo ser utilizada fora meu escopo (else)'; */
+/*     console.log(elseScope); */
+/*   } */
+/* } */
+/* testingScope(true); */
+
+//Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente
+//const oddsAndEvens = [13, 3, 4, 10, 7, 2]; 
+
+
+/* let numbers = [13, 3, 4, 10, 7, 2]; */
+/* numbers.sort((a, b) => a - b); */
+/* console.log(numbers); */
+/*  */
+/* console.log(numbers); */
+/* function fat (n) {  */
+/* let resultado = n; */
+/* for(let i = 1; i < n; i += 1){ */
+/* resultado = resultado * i; */
+/* } */
+/* return resultado; */
+/* } */
+/* console.log(fat(5)); */
+/*  */
+/* const factorial = number => { */
+/*     let result = 1; */
+/*   for (let index = 2; index <= number; index += 1) { */
+/*       result *= index; */
+/*       } */
+/*  */
+/*   return result; */
+/* } */
+/* console.log(factorial(9)); */
