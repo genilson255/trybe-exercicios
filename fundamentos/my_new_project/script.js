96b20d359183c6585c0d898b2624705a3b531324
Genilson <genilson255@hotmail.com>
2022-03-07 20:21:44 -0300

    Criei o arquivo script.test.js e o script.js

---
+++ fundamentos/my_new_project/script.js
@@ -0,0 +1,9 @@
+/* const sum = (a, b) => a + b; */
+/*  */
+/* test('sums two values', () => { */
+/*   expect(sum(2, 3)).toEqual(5); */
+/* }); */
+function sum(a, b) {
+  return a + b;
+}
+module.exports = sum;
